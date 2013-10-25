38ede7538be36177bc150cad0a53764b34a1c234
Marcio Manoel Sagaz <msg@ccd-7570l.certi.org>
2013-10-25 17:10:01 -0200

    Refeito Exercicio 6 para semaphore

--- Semafore/src/exec6Semaphore/Expositor.java
+++ Semafore/src/exec6Semaphore/Expositor.java
@@ -1,26 +1,31 @@
 package exec6Semaphore;
 
+import java.util.concurrent.Semaphore;
+
 public class Expositor extends Thread {
 
 	SalaExposicao salaExposicao;
 	int tempoExposicao;
-
-	public Expositor(SalaExposicao salaExposicao, int tempoExposicao) {
+	Semaphore semaphore;
+	public Expositor(SalaExposicao salaExposicao, Semaphore semaphore) {
 		this.salaExposicao = salaExposicao;
-		this.tempoExposicao = tempoExposicao;
+		this.semaphore = semaphore;
+		
 	}
 
 	@Override
 	public void run() {
 		while (true) {
 			// System.out.println("Estou em looping sou Expositor");
-			salaExposicao.openExposicao();
 			try {
-				sleep(tempoExposicao);
+				salaExposicao.openExposicao();
+				semaphore.acquire();
+				//sleep(1000);
 			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			salaExposicao.encerrarExposicao();
+			salaExposicao.sairDaExposicao();
 		}
 	}
 
--- Semafore/src/exec6Semaphore/Visitante.java
+++ Semafore/src/exec6Semaphore/Visitante.java
@@ -1,31 +1,13 @@
 package exec6Semaphore;
 
-public class Visitante extends Thread {
 
-	SalaExposicao sala;
-	boolean saiu;
+public class Visitante {
 
-	public Visitante(SalaExposicao sala) {
-		this.sala = sala;
-	}
-
-	@Override
-	public void run() {
-		entra();
-	}
-
-	private void entra() {
-		try {
-			while (!sala.entrarNaExposicao(this)) {
-				;
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	public Visitante() {
 	}
 
 	public void sai() {
 		System.out.println("$ Saindo da Exposição. Sou o Visitante :"
-				+ this.getId());
+				+ this.hashCode());
 	}
 }
